a204080de26f66cc0197cc48b108ae5605a5756f
joydebnath2288 <joydebnai@gmail.com>
2026-01-12 00:55:17 +0600

    Wishlist

--- models/Wishlist.php
+++ models/Wishlist.php
@@ -1,5 +1,5 @@
 <?php
-// models/Wishlist.php
+
 class Wishlist extends Model {
     private $table_name = "wishlist";
 
@@ -8,28 +8,25 @@ class Wishlist extends Model {
     }
 
     public function add($user_id, $menu_id) {
-        $query = "INSERT INTO " . $this->table_name . " (user_id, menu_id) VALUES (:user_id, :menu_id)";
+        $query = "INSERT INTO " . $this->table_name . " (user_id, menu_id) VALUES (?, ?)";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":user_id", $user_id);
-        $stmt->bindParam(":menu_id", $menu_id);
-        return $stmt->execute();
+        return $stmt->execute([$user_id, $menu_id]);
     }
 
     public function getByUserId($user_id) {
-        $query = "SELECT m.*, w.id as wishlist_id FROM " . $this->table_name . " w 
+        $query = "SELECT m.*, w.id as wishlist_id 
+                  FROM " . $this->table_name . " w 
                   JOIN menu m ON w.menu_id = m.id 
-                  WHERE w.user_id = :user_id";
+                  WHERE w.user_id = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":user_id", $user_id);
-        $stmt->execute();
+        $stmt->execute([$user_id]);
         return $stmt;
     }
+
     public function remove($user_id, $wishlist_id) {
-        $query = "DELETE FROM " . $this->table_name . " WHERE user_id = :user_id AND id = :wishlist_id";
+        $query = "DELETE FROM " . $this->table_name . " WHERE user_id = ? AND id = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":user_id", $user_id);
-        $stmt->bindParam(":wishlist_id", $wishlist_id);
-        return $stmt->execute();
+        return $stmt->execute([$user_id, $wishlist_id]);
     }
 }
 ?>
