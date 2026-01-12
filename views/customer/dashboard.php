97a74621b7470dfa27766cda20f055b3b1dfe562
Zunaid332 <zunaid332@gmail.com>
2026-01-13 00:31:44 +0600

    Delete views/customer/dashboard.php

--- views/customer/dashboard.php
+++
@@ -1,7 +0,0 @@
-<h3>My Dashboard</h3>
-<p>Welcome, <b><?php echo $_SESSION['user_name']; ?></b>!</p>
-<div class="card">
-    <h4>Recent Activity</h4>
-    <p>You have no recent orders.</p>
-    <a href="<?php echo BASE_URL; ?>index.php?page=home&action=menu" class="btn">Order Now</a>
-</div>
